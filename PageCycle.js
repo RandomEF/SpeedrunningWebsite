f70c3089f2ac038140cfe4edec8dc52f1fe8c111
RandomEF <eshan4u@outlook.com>
2024-01-03 19:28:09 +0000

    Cookie hack + font

--- PageCycle.js
+++ PageCycle.js
@@ -2,21 +2,24 @@ const pages = ["Speedrunning", "Disclaimer", "Categories", "Glitches", "TAS", "C
 const filename = location.pathname.split('/').pop().slice(0, -5);
 const backLink = document.getElementById("backLink");
 const forwardLink = document.getElementById("forwardLink");
-for (let i = 0; i < pages.length; i++){
+for (var i = 0; i < pages.length; i++){
     if (pages[i] == filename){
         if (i == 0){
             forwardLink.href = pages[i + 1] + ".html";
             forwardLink.innerHTML = pages[i + 1]
             backLink.innerHTML = '';
+            break
         } else if (i == pages.length - 1){
             backLink.href = pages[i - 1] + ".html";
             backLink.innerHTML = pages[i - 1];
             forwardLink.innerHTML = '';
+            break
         } else {
             backLink.href = pages[i - 1] + ".html";
             backLink.innerHTML = pages[i - 1];
             forwardLink.href = pages[i + 1] + ".html";
             forwardLink.innerHTML = pages[i + 1]
+            break
         }
     }
 }
@@ -25,3 +28,10 @@ sandwhichElement.style.backgroundColor = 'rgb(71, 71, 71)';
 sandwhichElement.style.borderColor = 'rgb(220, 220, 220)';
 const sandwhichLink = sandwhichElement.querySelector("a");
 sandwhichLink.style.color = 'rgb(220, 220, 220)';
+
+function GetNextPage(){
+    return pages[i + 1];
+}
+function GetNextLink(){
+    return pages[i + 1] + ".html";
+}
